4bc75acbc92378f3795b51826913919b57e0b486
fbragee <145098995+fbragee@users.noreply.github.com>
2024-01-31 08:01:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+add = (a, b) => {
+	return a + b;
+};
+subtract = (a, b) => {
+	return a - b;
+};
+multiply = (a, b) => {
+	return a * b;
+};
+divide = (a, b) => {
+	return a / b;
+};
+
+let firstNum;
+let secondNum;
+let operator;
